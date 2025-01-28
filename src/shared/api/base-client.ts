088a93801a4930778a8af988324ffcceef5e8627
Sergey <galaktionov@4dk.ru>
2025-01-28 09:18:46 +0300

    fix: import env

--- src/shared/api/base-client.ts
+++ src/shared/api/base-client.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import * as process from 'node:process';
+import * as process from 'process';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL
