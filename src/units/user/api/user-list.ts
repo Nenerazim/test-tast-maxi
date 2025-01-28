f0086807e542c5e66af87508980fdb594aca69b3
Sergey <galaktionov@4dk.ru>
2025-01-28 09:11:16 +0300

    fix: rebase on env

--- src/units/user/api/user-list.ts
+++ src/units/user/api/user-list.ts
@@ -2,7 +2,5 @@ import {SharedApi, SharedLib} from '@shared';
 import {UserType} from '@units/user';
 
 export const getUserList = async () => {
-  return await SharedApi.baseClient.get<SharedLib.Types.BaseErrorResponseType, UserType.TUserEntity[], undefined>(
-    'https://jsonplaceholder.typicode.com/users'
-  );
+  return await SharedApi.baseClient.get<SharedLib.Types.BaseErrorResponseType, UserType.TUserEntity[], undefined>('/users');
 };
